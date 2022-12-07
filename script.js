9c75a0ea56d76a730da7a643efb39286769fd72c
ThomasLuton <luton.thomas60@gmail.com>
2022-12-07 16:57:47 +0100

    fix novalidate issue and refactor

--- script.js
+++ script.js
@@ -1,72 +1,57 @@
+const toastSucces = document.getElementById('liveToast');
+const toast = new bootstrap.Toast(toastSucces);
 
-(() => {
-    //Création du toast
-    const toastSucces = document.getElementById('liveToast');
-    const toast = new bootstrap.Toast(toastSucces);
-    //On récupère le formulaire 
-    const forms = document.querySelectorAll('.needs-validation')
+const forms = document.querySelectorAll('.needs-validation')
+document.getElementById("save").addEventListener('click', event => {
+    forms[0].classList.add("was-validated");
+    if (forms[0].checkValidity()) {
+        toast.show();
+        event.preventDefault();
+    }
+})
 
-    // Loop over them and prevent submission
-    Array.from(forms).forEach(form => {
-        form.addEventListener('submit', event => {
-            if (!form.checkValidity()) {
-                event.preventDefault()
-                event.stopPropagation()
-                //Si on a un soucis on appelle la fonction
-                //check qui nous permet de voir si c'est un 
-                //champ non rempli ou une autre erreur
-                check();
+
+Array.from(forms[0]).forEach(input => {
+    const id = input.getAttribute("id");
+    const type = input.getAttribute("type");
+
+    const element = document.getElementById(`${id}`);
+    if (type == "text") {
+        element.addEventListener('invalid', event => {
+            if (element.value == "") {
+                document.getElementById(`${id}-feedback`).innerHTML = "Ce champ est obligatoire";
             } else {
-                //Si tout est valide on affiche le toast
-                toast.show();
+                document.getElementById(`${id}-feedback`).innerHTML = "Trop de caractères";
             }
-            //Ajoute la classe bootstrap permettant d'afficher 
-            //le contenu personnaliser
-            form.classList.add('was-validated')
-
             event.preventDefault();
-        }, false)
-    })
-})()
+        });
+    }
+    if (id == "rate") {
+        element.addEventListener('invalid', event => {
+            if (element.value == "") {
+                document.getElementById(`${id}-feedback`).innerHTML = "Ce champ est obligatoire";
+            } else {
+                document.getElementById(`${id}-feedback`).innerHTML = "Doit être positif";
+            }
+            event.preventDefault();
+        });
+    }
+    if (type == "date") {
+        element.addEventListener('invalid', event => {
+            if (element.valueAsDate == undefined) {
+                document.getElementById(`${id}-feedback`).innerHTML = "Ce champ est obligatoire";
+            } else {
+                document.getElementById(`${id}-feedback`).innerHTML = "Doit être égale ou supérieur à aujourd'hui";
+            }
+            event.preventDefault();
+        });
+    }
+    if (type == "select") {
+        element.addEventListener('invalid', event => {
+            document.getElementById(`${id}-feedback`).innerHTML = "Ce champ est obligatoire";
+            event.preventDefault();
+        });
+    }
+});
 
-function check() {
-    checkName();
-    checkDate();
-    checkTarif();
-    checkDescription();
-}
-function checkName() {
-    const input = document.getElementById("nom").value;
-    if (input.length > 255) {
-        document.getElementById("nom-feedback").innerHTML = "Nom trop long";
-    }
-    if (!input) {
-        document.getElementById("nom-feedback").innerHTML = "Ce champ est obligatoire";
-    }
-}
 
-function checkDate() {
-    const input = document.getElementById("date").valueAsDate;
-    const date = new Date(input);
-    const today = new Date();
-    if (input && date < today) {
-        document.getElementById("date-feedback").innerHTML = "Doit être égale ou supérieur à aujourd'hui";
-    }
-}
-
-function checkTarif() {
-    let input = document.getElementById("tarif").value;
-    input = parseInt(input, 10);
-    if (input <= 0) {
-        document.getElementById("tarif-feedback").innerHTML = "Doit être positif";
-    }
-}
-function checkDescription() {
-    const input = document.getElementById("description").value;
-    if (input.length > 1000) {
-        document.getElementById("description-feedback").innerHTML = "Description trop longue";
-    }
-    if (!input) {
-        document.getElementById("description-feedback").innerHTML = "Ce champ est obligatoire";
-    }
-}

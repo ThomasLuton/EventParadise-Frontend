c98f5e12ebd0370471b5658d7b5ab619104e345e
ThomasLuton <luton.thomas60@gmail.com>
2022-12-09 17:04:00 +0100

    arrrrrgh

--- script.js
+++ script.js
@@ -1,60 +1,70 @@
 const toastSucces = document.getElementById('liveToast');
 const toast = new bootstrap.Toast(toastSucces);
 
+const form = document.querySelector("form")
 
-
-
-const forms = document.querySelectorAll('.needs-validation')
-document.getElementById("save").addEventListener('click', event => {
-    forms[0].classList.add("was-validated");
-    if (forms[0].checkValidity()) {
-        toast.show();
-        event.preventDefault();
-    }
-})
-
-
-Array.from(forms[0]).forEach(input => {
-    const id = input.getAttribute("id");
-    const type = input.getAttribute("type");
-
-    const element = document.getElementById(`${id}`);
-    if (type == "text") {
-        element.addEventListener('invalid', event => {
-            if (element.value == "") {
-                document.getElementById(`${id}-feedback`).innerHTML = "Ce champ est obligatoire";
-            } else {
-                document.getElementById(`${id}-feedback`).innerHTML = "Trop de caractères";
-            }
-            event.preventDefault();
-        });
-    }
-    if (id == "rate") {
-        element.addEventListener('invalid', event => {
-            if (element.value == "") {
-                document.getElementById(`${id}-feedback`).innerHTML = "Ce champ est obligatoire";
-            } else {
-                document.getElementById(`${id}-feedback`).innerHTML = "Doit être positif";
-            }
-            event.preventDefault();
-        });
-    }
-    if (type == "date") {
-        element.addEventListener('invalid', event => {
-            if (element.valueAsDate == undefined) {
-                document.getElementById(`${id}-feedback`).innerHTML = "Ce champ est obligatoire";
-            } else {
-                document.getElementById(`${id}-feedback`).innerHTML = "Doit être égale ou supérieur à aujourd'hui";
-            }
-            event.preventDefault();
-        });
-    }
-    if (type == "select") {
-        element.addEventListener('invalid', event => {
-            document.getElementById(`${id}-feedback`).innerHTML = "Ce champ est obligatoire";
-            event.preventDefault();
-        });
-    }
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    toast.show();
+    form.reset();
 });
 
+const options = {
+    title: "default",
+    trigger: "focus",
+    placement: "top"
+};
+const tooltips = [];
+for (let i = 0; i < form.length - 1; i++) {
+    const tmp = new bootstrap.Tooltip(form[i], options);
+    tmp.disable();
+    tooltips.push(tmp);
+}
 
+for (let i = 0; i < form.length - 1; i++) {
+
+    const element = form[i];
+    element.addEventListener('invalid', event => {
+        event.preventDefault();
+        const first = form.querySelector(":invalid");
+        if (element == first) {
+            tooltips[i].enable();
+            changeTooltip(tooltips[i], event);
+            element.focus();
+        }
+        element.addEventListener('change', event => {
+            event.preventDefault()
+            if (element.validity.valid) {
+                element.classList.remove("is-invalid")
+                element.classList.add("is-valid");
+
+            }
+            else if (element.classList.contains('is-invalid') && element.validity.valid) {
+                element.classList.remove("is-invalid")
+                element.classList.add("is-valid");
+
+            }
+            else {
+                element.classList.remove("is-valid");
+                element.classList.add("is-invalid");
+            }
+        });
+    });
+
+    // element.addEventListener('change', event => {
+    //     element.checkValidity()
+    //     if (element.checkValidity()) {
+    //         tooltips[i].disable();
+    //     }
+    // });
+}
+
+function changeTooltip(tooltip, event) {
+    if (event.target.validity.valueMissing) {
+        tooltip._config.title = "Champ obligatoire";
+    } else if (event.target.value < event.target.min && event.target.type === "number") {
+        tooltip._config.title = "Doit être positif";
+    } else if (event.target.type === "date") {
+        tooltip._config.title = "Doit être égale ou supérieure à aujourd'hui";
+    }
+}

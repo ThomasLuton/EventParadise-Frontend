4cab270b8ac6c497b790bd24af0d3d43476d4f81
ThomasLuton <luton.thomas60@gmail.com>
2022-12-13 12:04:32 +0100

    v7

--- script.js
+++ script.js
@@ -19,50 +19,55 @@ form.addEventListener('submit', event => {
 const options = {
     title: "default"
 };
-const tooltips = [6];
+
 
 for (let i = 0; i < form.length - 1; i++) {
 
     const element = form[i];
     const helpText = getHelpText(element);
+    let tooltip = "";
     element.addEventListener('invalid', event => {
         event.preventDefault();
-        const tooltip = new bootstrap.Tooltip(form[i], options);
-        tooltips[i] = tooltip;
+        tooltip = new bootstrap.Tooltip(form[i], options);
         const first = form.querySelector(":invalid");
         if (element == first) {
             element.focus();
         }
-        setInvalidity(element, helpText, tooltips[i], event);
+        setInvalidity(element, helpText, tooltip, event);
     });
     element.addEventListener('change', event => {
         event.preventDefault()
-        if (element.validity.valid) {
-            setValidity(element, helpText, tooltips[i]);
+        if (!element.validity.valid) {
+            console.log("invalid")
+            if (tooltip == "") {
+                tooltip = new bootstrap.Tooltip(form[i], options);
+            }
+            setInvalidity(element, helpText, tooltip, event);
         } else {
-            setInvalidity(element, helpText, tooltips[i], event);
+            setValidity(element, helpText, tooltip);
         }
     });
 }
 
 function changeTooltip(tooltip, event) {
-    if (event.target.validity.valueMissing) {
-        tooltip.setContent({ '.tooltip-inner': "Champ obligatoire" })
-    } else if (event.target.value < event.target.min && event.target.type === "number") {
-        tooltip.setContent({ '.tooltip-inner': "Doit être positif" })
-    } else if (event.target.type === "date") {
-        tooltip.setContent({ '.tooltip-inner': "Doit être égale ou supérieure à aujourd'hui" });
+    let content = "";
+    if (!(event.target.validity.valueMissing)) {
+        if (event.target.type === "number" && event.target.validity.rangeUnderflow) {
+            content = "Doit être positif";
+        }
+        if (event.target.type === "date" && event.target.validity.rangeUnderflow) {
+            content = "Doit être égale ou supérieure à aujourd'hui";
+        }
+    } else {
+        content = "Champ obligatoire";
     }
+    tooltip.setContent({ '.tooltip-inner': content });
 }
 
 function getHelpText(element) {
-    const parentElement = element.parentElement;
-    if (element.getAttribute("id") == "rate") {
-        const tmp = parentElement.parentElement;
-        return tmp.querySelector(".form-text");
-    } else {
-        return parentElement.querySelector(".form-text");
-    }
+    const id = element.getAttribute("id");
+    const helpId = `${id}Help`;
+    return document.getElementById(helpId);
 }
 
 function setValidity(element, helpText, tooltip) {
@@ -70,7 +75,10 @@ function setValidity(element, helpText, tooltip) {
     element.classList.add("is-valid");
     helpText.classList.remove("text-danger")
     helpText.classList.add("text-success");
-    tooltip.disable();
+    if (tooltip != "") {
+        tooltip.hide();
+        tooltip.disable();
+    }
 }
 
 function setInvalidity(element, helpText, tooltip, event) {

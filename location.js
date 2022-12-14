dfdf1570443eda04b80f63ecb6676dfc7146b6be
ThomasLuton <luton.thomas60@gmail.com>
2022-12-14 11:07:29 +0100

    front to back v1

---
+++ location.js
@@ -0,0 +1,27 @@
+async function get() {
+    const url = "http://localhost:8080/location";
+    const method = "GET";
+    const options = {
+        method: method
+    };
+    const response = await fetch(url, options);
+    const header = response.headers;
+    if (header.get("content-type") == "application/json") {
+        return await response.json();
+    }
+    return null;
+}
+async function render() {
+    const locations = await get();
+    let content = `
+<option selected value="">Choisir un location dans la liste</option>
+`;
+    locations.forEach(location => {
+        content += `<option value="${location.id}">${location.name}</option>`
+    });
+    const target = document.getElementById("location");
+    target.innerHTML = content;
+
+}
+render();
+
